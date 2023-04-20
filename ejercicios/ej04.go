10156a8644bf72e5d98a31ea7a9cb05ab85b8521
4tsel <axelbnovo@gmail.com>
2023-04-20 08:41:16 -0300

    Ejercicios 1 y 4 resueltos

--- ejercicios/ej04.go
+++ ejercicios/ej04.go
@@ -3,5 +3,11 @@ package ejercicios
 // Escriba un método recursivo que tome una cadena y
 // devuelva como resultado la cadena invertida.
 func Invertir(cadena string) string {
-	panic("Not implemented")
+	
+	if len(cadena) <= 1 {
+
+		return cadena
+	}
+
+	return string(cadena[len(cadena)-1]) + Invertir(cadena[:len(cadena)-1])
 }
