c53e46b1964f54cc28f9288a15f4879813f0545a
Axel N <axelbnovo@gmail.com>
2023-05-03 22:44:21 -0300

    Realizados algunos ejercicios

--- ejercicios/ej03.go
+++ ejercicios/ej03.go
@@ -5,5 +5,16 @@ package ejercicios
 // que es igual al número de unos en la representación binaria
 // de n/2, mas 1 si es impar.
 func CantidadDeUnos(n int) int {
-	panic("Not implemented")
+
+	if n == 0 {
+
+		return 0
+	}
+
+	if n == 1 {
+
+		return 1
+	}
+
+	return n%2 + CantidadDeUnos(n/2)
 }
--- ejercicios/ej05.go
+++ ejercicios/ej05.go
@@ -1,8 +1,20 @@
 package ejercicios
 
+// import "ejercicios/stack"
 // Escribir un método que calcule recursivamente cuántos
 // elementos hay en una pila y no altere el contenido de
 // la misma. La pila sólo tiene los métodos Push, Pop y isEmpty.
 func CantidadDeElementos(pila Stack) int {
-	panic("Not implemented")
+
+	if pila.IsEmpty(){
+
+		return 0
+	}
+
+	objeto, _ := pila.Pop()
+	tamano := CantidadDeElementos(pila) + 1
+	
+	pila.Push(objeto)
+
+	return tamano
 }
